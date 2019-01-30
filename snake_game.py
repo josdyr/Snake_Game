2f3a91b3ef2f115c2f1df88098cc9aa18d733828
Jostein Dyrseth <josdyr@gmail.com>
2019-01-30 20:05:00 +0000

    update_board() rename, added MAP-mapping, calc_distance, valid_move

--- snake_game.py
+++ snake_game.py
@@ -12,7 +12,7 @@ class Board:
 
     board = [[None for y in range(BOARD_SIZE)] for x in range(BOARD_SIZE)]
 
-    def set_snake(self, tail=None):
+    def update_board(self, tail=None):
         for body in snake.snake_body:
 
             # add body segments to board
@@ -57,6 +57,13 @@ class Board:
 class Snake:
     """Holds a list of all snake positions"""
 
+    MAP = {
+        'up': [-1,0],
+        'down': [1,0],
+        'right': [0,1],
+        'left': [0,-1],
+    }
+
     def __init__(self, INIT_SNAKE_POSITIONS):
         self.snake_body = INIT_SNAKE_POSITIONS
 
@@ -66,13 +73,31 @@ class Snake:
     def add_body(self, body):
         self.snake_body.append(body)
 
-    def move(self, destination):
+    def calc_destination(self, direction):
+        """add diff_xy to snake_head"""
+        diff_xy = self.MAP[direction]; print("diff_xy: {}".format(diff_xy))
+        snake_head = self.snake_body[0]; print("snake_head: {}".format(snake_head))
+        destination = [a_i + b_i for a_i, b_i in zip(diff_xy, snake_head)]
+        print("destination: {}".format(destination))
+        return destination
+
+    def valid_move(self, destination):
+        if 0 <= destination[0] and destination[1] <= 9:
+            return True
+        else:
+            return False
+
+    def move(self, direction):
+        destination = self.calc_destination(direction)
         tail = snake.snake_body[-1]
         del snake.snake_body[-1]
-        try:
+        if not self.valid_move(destination):
+            game.game_over = True
+            tail = None
+            print("game_over={}".format(game.game_over))
+            print("APP: OutOfBoundsError: destination={} is outside the board.".format(destination))
+        else:
             snake.snake_body.insert(0, destination)
-        except Exception as e:
-            raise
         return tail
 
     def wall_collision(self):
@@ -98,37 +123,28 @@ class Apple:
 class Game:
     """Holds the board"""
 
+    game_over = False
     board = Board()
+    loop_count = 0
+    current_move = ''
 
     def start(self):
+        while not self.game_over and self.loop_count <= len(MOVES)-1:
+            tail = snake.move(MOVES[self.loop_count]) # TODO: don't return tail, and set it rather from method
 
-        i = 0
-        running = True
-        while running:
-            for body in bodies_to_be_added:
+            game.board.update_board(tail=tail)
+            game_states.append(game.board)
 
-                tail = snake.move(MOVES[i])
-                game.board.set_snake(tail=tail)
-                game_states.append(game.board)
+            game.board.draw_board()
+            print(repr(snake))
+            print(str(game.board))
+            print("len(game_states): {}".format(len(game_states)))
 
-                game.board.draw_board()
-                print(repr(snake))
-                print(str(game.board))
-                print(game.board)
-
-                time.sleep(1)
-                i += 1
-
-            game.board.set_apple()
             time.sleep(1)
-            break
+            self.loop_count += 1
 
 
-bodies_to_be_added = [[3,0],[4,0],[5,0],[6,0]]
-MOVES = [[0,1],[0,2],[1,2],[2,2]]
-# MOVES = ['r','f','r','f']
-
-# holds snapshots of all previous game states
+MOVES = ['right','right','down']
 game_states = []
 
 snake = Snake(INIT_SNAKE_POSITIONS)
@@ -136,7 +152,7 @@ game = Game()
 apple = Apple()
 
 # os.system('clear')
-game.board.set_snake()
+game.board.update_board()
 game.board.set_apple()
 game.board.draw_board()
 print(repr(snake))
